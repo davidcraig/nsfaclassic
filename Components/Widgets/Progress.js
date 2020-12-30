6d3d973abdf9f13fe388186639351b2c8cff98c9
davidcraig <davidcraig1986@gmail.com>
2020-12-30 14:57:03 +0000

    Refactor progression statuses.

--- Components/Widgets/Progress.js
+++ Components/Widgets/Progress.js
@@ -1,16 +1,16 @@
 import { Card } from '@davidcraig/react-bulma'
 
 const castleNathria = [
-  { name: 'Shriekwing',            slug: 'cn-shriekwing', normal: false, heroic: false },
-  { name: 'Huntsman Altimor',      slug: 'cn-altimor',    normal: false, heroic: false },
-  { name: "Artificer Xy'mox",      slug: 'cn-xymox',      normal: false, heroic: false },
-  { name: "Sun King's Salvation",  slug: 'cn-sunking',    normal: false, heroic: false },
-  { name: 'Council of Blood',      slug: 'cn-council',    normal: false, heroic: false },
-  { name: 'Hungering Destroyer',   slug: 'cn-hungering',  normal: false, heroic: false },
-  { name: 'Lady Darkvein',         slug: 'cn-darkvein',   normal: false, heroic: false },
-  { name: 'Sludgefist',            slug: 'cn-sludgefist', normal: false, heroic: false },
-  { name: 'Stone Legion Generals', slug: 'cn-generals',   normal: false, heroic: false },
-  { name: 'Sire Denathrius',       slug: 'cn-denathrius', normal: false, heroic: false },
+  { name: 'Shriekwing',            slug: 'cn-shriekwing', normal: false, heroic: false, mythic: false },
+  { name: 'Huntsman Altimor',      slug: 'cn-altimor',    normal: false, heroic: false, mythic: false },
+  { name: "Artificer Xy'mox",      slug: 'cn-xymox',      normal: false, heroic: false, mythic: false },
+  { name: "Sun King's Salvation",  slug: 'cn-sunking',    normal: false, heroic: false, mythic: false },
+  { name: 'Council of Blood',      slug: 'cn-council',    normal: false, heroic: false, mythic: false },
+  { name: 'Hungering Destroyer',   slug: 'cn-hungering',  normal: false, heroic: false, mythic: false },
+  { name: 'Lady Darkvein',         slug: 'cn-darkvein',   normal: false, heroic: false, mythic: false },
+  { name: 'Sludgefist',            slug: 'cn-sludgefist', normal: false, heroic: false, mythic: false },
+  { name: 'Stone Legion Generals', slug: 'cn-generals',   normal: false, heroic: false, mythic: false },
+  { name: 'Sire Denathrius',       slug: 'cn-denathrius', normal: false, heroic: false, mythic: false },
 ]
 
 function getCurrentRaidTier() {
@@ -18,10 +18,17 @@ function getCurrentRaidTier() {
 }
 
 function getStatus (progression) {
-  if (progression.mythic) { return 'Killed: N, HC, M' }
-  if (progression.heroic) { return 'Killed: N, HC' }
-  if (progression.normal) { return 'Killed: N' }
-  return 'Not yet killed.'
+  if (!progression.normal) {
+    return 'Not yet killed'
+  }
+
+  let str = 'Killed: '
+  let kills = []
+
+  if (progression.normal) { kills.push('N') }
+  if (progression.heroic) { kills.push('HC') }
+  if (progression.mythic) { kills.push('M') }
+  return str + kills.join(', ')
 }
 
 function getStatusClass (progression) {
