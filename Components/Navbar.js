f8ce9c838606d7650bb0a239357915a3a9119639
davidcraig <davidcraig1986@gmail.com>
2020-12-30 17:02:04 +0000

    Add link to raider.io

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,9 +1,15 @@
-import { Navbar, Brand } from '@davidcraig/react-bulma'
+import { Navbar, Brand, Start, End, Item } from '@davidcraig/react-bulma'
 
 export default function Navigation() {
   return (
     <Navbar>
       <Brand title='Not Safe for Azeroth' />
+      <End>
+        <Item
+          href='https://raider.io/guilds/eu/silvermoon/Not%20Safe%20for%20Azeroth'
+          title='Raider.IO'
+        />
+      </End>
     </Navbar>
   )
 }
