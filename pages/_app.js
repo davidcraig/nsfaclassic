3293a43a3f78fc1ec03f488e398657f119ed6f7a
davidcraig <davidcraig1986@gmail.com>
2020-12-30 14:13:55 +0000

    Add navbar.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import { Navbar, Brand } from '@davidcraig/react-bulma'
 import '../styles/globals.css'
 import '../styles/app.scss'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar>
+        <Brand title='Not Safe for Azeroth' />
+      </Navbar>
+      <div className='container'>
+        <div className='content'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default MyApp
