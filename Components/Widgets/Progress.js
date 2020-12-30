9ae708b9e9788b37a468fa916e987658cc1c2afe
davidcraig <davidcraig1986@gmail.com>
2020-12-30 16:01:01 +0000

    Extract raid progression data into data folder.

--- Components/Widgets/Progress.js
+++ Components/Widgets/Progress.js
@@ -1,17 +1,5 @@
 import { Card } from '@davidcraig/react-bulma'
-
-const castleNathria = [
-  { name: 'Shriekwing',            slug: 'cn-shriekwing', normal: false, heroic: false, mythic: false },
-  { name: 'Huntsman Altimor',      slug: 'cn-altimor',    normal: false, heroic: false, mythic: false },
-  { name: "Artificer Xy'mox",      slug: 'cn-xymox',      normal: false, heroic: false, mythic: false },
-  { name: "Sun King's Salvation",  slug: 'cn-sunking',    normal: false, heroic: false, mythic: false },
-  { name: 'Council of Blood',      slug: 'cn-council',    normal: false, heroic: false, mythic: false },
-  { name: 'Hungering Destroyer',   slug: 'cn-hungering',  normal: false, heroic: false, mythic: false },
-  { name: 'Lady Darkvein',         slug: 'cn-darkvein',   normal: false, heroic: false, mythic: false },
-  { name: 'Sludgefist',            slug: 'cn-sludgefist', normal: false, heroic: false, mythic: false },
-  { name: 'Stone Legion Generals', slug: 'cn-generals',   normal: false, heroic: false, mythic: false },
-  { name: 'Sire Denathrius',       slug: 'cn-denathrius', normal: false, heroic: false, mythic: false },
-]
+import { castleNathria } from '../../data/shadowlands/raids'
 
 function getCurrentRaidTier() {
   return castleNathria
---
+++ data/shadowlands/raids.js
@@ -0,0 +1,42 @@
+export const castleNathria = [
+  {
+    name: 'Shriekwing', slug: 'cn-shriekwing',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Huntsman Altimor', slug: 'cn-altimor',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: "Artificer Xy'mox", slug: 'cn-xymox',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: "Sun King's Salvation", slug: 'cn-sunking',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Council of Blood', slug: 'cn-council',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Hungering Destroyer', slug: 'cn-hungering',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Lady Darkvein', slug: 'cn-darkvein',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Sludgefist', slug: 'cn-sludgefist',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Stone Legion Generals', slug: 'cn-generals',
+    normal: false, heroic: false, mythic: false
+  },
+  {
+    name: 'Sire Denathrius', slug: 'cn-denathrius',
+    normal: false, heroic: false, mythic: false
+  },
+]
