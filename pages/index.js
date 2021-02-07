93f65ef6bc7c7b6877b22ad72fcdcb8d2fbdf397
davidcraig <davidcraig1986@gmail.com>
2021-02-07 23:44:55 +0000

    Use @davidcraig/react-schema for Organization.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
 import React from 'react'
+import { Organization } from '@davidcraig/react-schema'
 import PageWithWidgets from '../Components/PageWithWidgets'
 
 export default function Home() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "Organization",
-    "url": "https://notsafeforazeroth.netlify.app/",
-    "name": "Not Safe for Azeroth",
-    "description": "A World of Warcraft guild on Silvermoon EU."
-  }
-
   return <PageWithWidgets>
     <h1 className='h1'>&lt;Not Safe for Azeroth&gt;</h1>
     <p>A raiding guild aiming at heroic progression, the name is a play on the phrase "Not Safe for Work" as we are a dark humoured guild so we recruit only players who have a sense of humour and aren't easily offended (aka no snowflakes).</p>
-    <script
-      type='application/ld+json'
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    <Organization
+      name='Not Safe for Azeroth'
+      description='A World of Warcraft guild on Silvermoon EU.'
+      url='https://notsafeforazeroth.netlify.app/'
     />
   </PageWithWidgets>
 }
