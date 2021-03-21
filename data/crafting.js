1b0f595c3a6da590c29437ec0d19c8365c6718a8
davidcraig <davidcraig1986@gmail.com>
2021-03-21 00:31:30 +0000

    Remove lego and shadowlands info.

--- data/crafting.js
+++ data/crafting.js
@@ -1,6 +1,4 @@
 import { findByName } from './characters'
-import Tailoring from './shadowlands/crafting/legendaries/tailoring'
-import Leatherworking from './shadowlands/crafting/legendaries/leatherworking'
 
 export const guildCrafting = {
   tailoring: { name: 'Tailoring', crafters: [ findByName() ] },
@@ -11,7 +9,7 @@ export const guildCrafting = {
   engineering: { name: 'Engineering', crafters: [] },
 
   alchemy: { name: 'Alchemy', crafters: [] },
-  jewelcrafting: { name: 'Jewelcrafting', crafters: [] },
+  // jewelcrafting: { name: 'Jewelcrafting', crafters: [] },
 
   enchanting: { name: 'Enchanting', crafters: [] },
 }
@@ -21,18 +19,7 @@ export const legendaries = {
     name: 'Tailoring',
     crafters: [
       {
-        crafter: findByName('Snipermagi'),
-        legendaries: [
-          { legendary: Tailoring.GRIM_VEILED_BELT, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_BRACERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_CAPE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_HOOD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_MITTENS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_PANTS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_SANDALS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_ROBE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_SHOULDERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-        ]
+        crafter: findByName('Sniperdot'),
       }
     ]
   },
@@ -44,34 +31,14 @@ export const legendaries = {
     crafters: [
       {
         crafter: findByName('Tyriant'),
-        legendaries: [
-          { legendary: Leatherworking.UMBRAHIDE_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_LEGGINGS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-
-          { legendary: Leatherworking.BONESHATTER_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_GREAVES, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-        ]
       }
     ]
   },
-  jewelcrafting: {
-    name: 'Jewelcrafting'
-  }
+  // jewelcrafting: {
+  //   name: 'Jewelcrafting'
+  // }
 }
 
 export default {
-  guildCrafting,
-  legendaries
+  guildCrafting
 }
